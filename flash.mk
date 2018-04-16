8351dacb70b4f9608dc69495aa69f86f515fe67c
Daniel Mazzer <dmazzer@gmail.com>
2018-04-16 07:05:35 -0300

    created automated make to program 3 boards

--- flash.mk
+++ flash.mk
@@ -37,4 +37,29 @@ flash_softdevice:
 	@echo Flashing: s140_nrf52840_5.0.0-3.alpha_softdevice.hex
 	nrfjprog -f nrf52 --chiperase --program /home/mazzer/work/nordic/sdk/nrf5_SDK_for_Mesh_v1.0.1_src/external/softdevice/s140_5.0.0-3.alpha/s140_nrf52840_5.0.0-3.alpha_softdevice.hex
 	nrfjprog --reset -f nrf52
+	
+flash_inatel_env: all
+	@echo Flashing Client: s140_nrf52840_5.0.0-3.alpha_softdevice.hex
+	nrfjprog -f nrf52 --chiperase -s 683232245 --program /home/mazzer/work/nordic/sdk/nrf5_SDK_for_Mesh_v1.0.1_src/external/softdevice/s140_5.0.0-3.alpha/s140_nrf52840_5.0.0-3.alpha_softdevice.hex
+	nrfjprog --reset -f nrf52 -s 683232245
+
+	@echo Flashing Client: inatel_light_switch_client_nrf52840_xxAA_s140_5.0.0-3.alpha.hex
+	nrfjprog -f nrf52 --sectorerase -s 683232245 --program ./examples/inatel_light_switch/client/inatel_light_switch_client_nrf52840_xxAA_s140_5.0.0-3.alpha.hex
+	nrfjprog --reset -f nrf52 -s 683232245
+
+	@echo Flashing Server 1: s140_nrf52840_5.0.0-3.alpha_softdevice.hex
+	nrfjprog -f nrf52 --chiperase -s 683358487 --program /home/mazzer/work/nordic/sdk/nrf5_SDK_for_Mesh_v1.0.1_src/external/softdevice/s140_5.0.0-3.alpha/s140_nrf52840_5.0.0-3.alpha_softdevice.hex
+	nrfjprog --reset -f nrf52 -s 683358487
+
+	@echo Flashing Server 1: inatel_light_switch_server_nrf52840_xxAA_s140_5.0.0-3.alpha.hex
+	nrfjprog -f nrf52 --sectorerase -s 683358487 --program ./examples/inatel_light_switch/server/inatel_light_switch_server_nrf52840_xxAA_s140_5.0.0-3.alpha.hex
+	nrfjprog --reset -f nrf52 -s 683358487
+
+	@echo Flashing Server 2: s140_nrf52840_5.0.0-3.alpha_softdevice.hex
+	nrfjprog -f nrf52 --chiperase -s 683111702 --program /home/mazzer/work/nordic/sdk/nrf5_SDK_for_Mesh_v1.0.1_src/external/softdevice/s140_5.0.0-3.alpha/s140_nrf52840_5.0.0-3.alpha_softdevice.hex
+	nrfjprog --reset -f nrf52 -s 683111702
+
+	@echo Flashing Server 2: inatel_light_switch_server_nrf52840_xxAA_s140_5.0.0-3.alpha.hex
+	nrfjprog -f nrf52 --sectorerase -s 683111702 --program ./examples/inatel_light_switch/server/inatel_light_switch_server_nrf52840_xxAA_s140_5.0.0-3.alpha.hex
+	nrfjprog --reset -f nrf52 -s 683111702
 
