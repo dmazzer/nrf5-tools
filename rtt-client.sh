095bd86498087e435f4d2fea59e2c399b9835669
Daniel Mazzer <dmazzer@gmail.com>
2018-04-19 07:45:32 -0300

    include another device and labels for terminator screen

--- rtt-client.sh
+++ rtt-client.sh
@@ -2,9 +2,11 @@
 
 (terminator -e 'JLinkExe -device NRF52840_XXAA -if SWD -speed 12000 -autoconnect 1 -SelectEmuBySN 683232245 -RTTTelnetport 19021') &
 (terminator -e 'JLinkExe -device NRF52840_XXAA -if SWD -speed 12000 -autoconnect 1 -SelectEmuBySN 683358487 -RTTTelnetport 19022') &
+(terminator -e 'JLinkExe -device NRF52840_XXAA -if SWD -speed 12000 -autoconnect 1 -SelectEmuBySN 683111702 -RTTTelnetport 19023') &
 
-(terminator -e 'JLinkRTTClient -RTTTelnetport 19021') &
-(terminator -e 'JLinkRTTClient -RTTTelnetport 19022') &
+(terminator -T 'Client' -e 'JLinkRTTClient -RTTTelnetport 19021') &
+(terminator -T 'Server 1' -e 'JLinkRTTClient -RTTTelnetport 19022') &
+(terminator -T 'Server 2' -e 'JLinkRTTClient -RTTTelnetport 19023') &
 
 
 
